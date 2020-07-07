01b60e40465320770f30cd2419b215585e0f5631
leet08 <leet08@gmail.com>
2020-07-07 12:05:49 -0400

    lab 2

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ model = joblib.load('./lab week 2/regr_lab2_prob1.pkl')
 
 # Make prediction - features = ['BEDS', 'BATHS', 'SQFT', 'AGE', 'LOTSIZE', 'GARAGE']
 prediction = model.predict([[4, 2.5, 3005, 15, 17903.0, 1]])[0][0].round(1)
-prediction = (prediction + 23000)*(999900-23000)
+prediction = prediction *(999900-23000) +23000
 prediction = str(prediction)
 
 	# Load ML model
@@ -18,9 +18,10 @@ model2 = joblib.load('./lab week 2/regr_lab2_prob2.pkl')
 
 # Make prediction - features = ['BEDS', 'BATHS', 'SQFT', 'AGE', 'LOTSIZE', 'GARAGE']
 prediction2 = model2.predict([[4, 2.5, 3005, 15, 17903.0, 1]])[0].round(1)
-prediction2 = (prediction2 + 23000)*(999900-23000)
+prediction2 = prediction2 *(999900-23000) +23000
 prediction2 = str(prediction2)
 
 @app.route('/')
 def predict1():
 	return 'Prediction for linear regression is $' + prediction + ' and prediction for decision tree regression is $' + prediction2
+
