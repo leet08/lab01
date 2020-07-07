b497963d637d228bd863f0e544f65dfec4772703
leet08 <leet08@gmail.com>
2020-07-06 20:11:38 -0400

    lab 2

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 from flask import Flask
+#from joblib import joblib
+import joblib as joblib
+
 app = Flask(__name__)
 
+
+	# Load ML model
+model = joblib.load('./lab week 2/regr_lab2_prob1.pkl')
+
+# Make prediction - features = ['BEDS', 'BATHS', 'SQFT', 'AGE', 'LOTSIZE', 'GARAGE']
+prediction = model.predict([[4, 2.5, 3005, 15, 17903.0, 1]])[0][0].round(1)
+prediction = str(prediction)
+
+	# Load ML model
+model2 = joblib.load('./lab week 2/regr_lab2_prob2.pkl')
+
+# Make prediction - features = ['BEDS', 'BATHS', 'SQFT', 'AGE', 'LOTSIZE', 'GARAGE']
+prediction2 = model2.predict([[4, 2.5, 3005, 15, 17903.0, 1]]).round(1)
+prediction2 = str(prediction2)
+
 @app.route('/')
-def hello_world():
-	return 'Hello, World!'
+def predict1():
+	return 'Prediction for linear regression' + prediction
+@app.route('/')
+def predict2():
+	return 'Prediction for decision tree regression' + prediction2
